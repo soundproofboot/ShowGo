bf91cd07e7880c0662aeb98163acb4af840bbbbf
Colin Bares <colinedwinbares@gmail.com>
2026-02-20 18:09:58 -0600

    Add basic version of User and cleandb.sql to test set up

--- src/main/java/com/showgo/entity/User.java
+++ src/main/java/com/showgo/entity/User.java
@@ -1,4 +1,77 @@
 package com.showgo.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * The type User.
+ */
+@Entity(name = "User")
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param id       the id
+     * @param username the username
+     */
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username=" + username + '}';
+    }
 }
---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,4 @@
+drop table if exists user;
+create table user (id int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(45));
+INSERT INTO user (id, username) VALUES (1, 'user1'), (2, 'user2'), (3, 'user3');
+SELECT * FROM user;
