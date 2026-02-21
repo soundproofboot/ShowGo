80dcc1155dd583e9ec190f1dc360cafa56e685b7
Colin Bares <colinedwinbares@gmail.com>
2026-02-20 18:28:24 -0600

    Update PropertiesLoader

--- src/main/java/com/showgo/util/PropertiesLoader.java
+++ src/main/java/com/showgo/util/PropertiesLoader.java
@@ -1,20 +1,32 @@
 package com.showgo.util;
+
 import java.io.*;
 import java.util.*;
 
 /**
- * Interface for loading properties files. Based on interface from Madison College's Adv Java
- *
- * @throws IOException - when the properties file cannot be read
- *
+ * This interface contains a default method that can be used anywhere a Properties
+ * object is needed to be loaded.
  * @author Eric Knapp
- * @author Paula Waite
+ *
  */
-public interface PropertiesLoader{
+public interface PropertiesLoader {
 
-    default Properties loadProperties(String propertiesFilePath) throws IOException {
+    /**
+     * This default method will load a properties file into a Properties instance
+     * and return it.
+     * @param propertiesFilePath a path to a file on the java classpath list
+     * @return a populated Properties instance or an empty Properties instance if
+     * the file path was not found.
+     */
+    default Properties loadProperties(String propertiesFilePath){
         Properties properties = new Properties();
-        properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        try {
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
         return properties;
     }
 }
