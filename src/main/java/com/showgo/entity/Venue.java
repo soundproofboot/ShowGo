acec763083e253a053c8c37ae9916e48823a878b
Colin Bares <colinedwinbares@gmail.com>
2026-03-04 16:40:31 -0600

    Create new entity for Venue with basic information, update db

---
+++ src/main/java/com/showgo/entity/Venue.java
@@ -0,0 +1,92 @@
+package com.showgo.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Objects;
+
+/**
+ * Represents the venue table
+ */
+@Entity(name = "Venue")
+@Table(name = "venue")
+public class Venue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * Instantiates a new Venue.
+     */
+    public Venue() {
+    }
+
+    /**
+     * Instantiates a new Venue.
+     *
+     * @param name the name
+     */
+    public Venue(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Venue{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Venue venue = (Venue) o;
+        return id == venue.id && Objects.equals(name, venue.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
