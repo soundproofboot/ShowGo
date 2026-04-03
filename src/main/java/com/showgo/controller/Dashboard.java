a974512d9c8fc06cce5c1271f4ede5ac7012319c
Colin Bares <colinedwinbares@gmail.com>
2026-04-03 16:29:11 -0500

    Create Dashboard controller

---
+++ src/main/java/com/showgo/controller/Dashboard.java
@@ -0,0 +1,33 @@
+package com.showgo.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = { "/dashboard"}
+)
+public class Dashboard extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String title = "Dashboard";
+        req.setAttribute("title", title);
+
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user") != null) {
+            System.out.println("there is a user in session");
+            System.out.println(session.getAttribute("user"));
+
+        } else {
+            System.out.println("there is not a user in session");
+
+        }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/dashboard.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
